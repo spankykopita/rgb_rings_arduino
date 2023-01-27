9a21363556f348490d3dec207132abd303924dd2
Christopher Powers <canasclone@gmail.com>
2023-01-26 22:41:53 -0800

    Add in bluefairy scheduler

--- rgb_rings_arduino/rgb_rings_arduino.ino
+++ rgb_rings_arduino/rgb_rings_arduino.ino
@@ -1,8 +1,20 @@
 #include "FastLED.h"
+#include <bluefairy.h>
+
+bluefairy::Scheduler scheduler;
 
 CRGB leds[1];
-void setup() { FastLED.addLeds<NEOPIXEL, 1>(leds, 1); }
-void loop() { 
-  leds[0] = CRGB::White; FastLED.show(); delay(30); 
-  leds[0] = CRGB::Black; FastLED.show(); delay(30);
+boolean showWhite = 1;
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, 1>(leds, 1);
+  scheduler.every(500, [](){
+    showWhite = !showWhite;
+    leds[0] = showWhite ? CRGB::White : CRGB::Black;
+    FastLED.show();
+  });
+}
+
+void loop() {
+  scheduler.loop();
 }
